d034183f82d60053c2982161e8eee8a0dbc39193
Humberto Ramone <humberto.ramone@csprime.com.br>
2024-06-19 18:17:26 -0300

    feat: Inclusao da listagem de pacientes

---
+++ ProConsulta/Components/Pages/Pacientes/Index.razor.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using ProConsulta.Models;
+using ProConsulta.Repositories.Pacientes;
+
+namespace ProConsulta.Components.Pages.Pacientes;
+
+public class IndexPage : ComponentBase
+{
+    [Inject]
+    public IPacienteRepository repository { get; set; } = null!;
+
+    [Inject]
+    public IDialogService dialog { get; set; } = null!;
+
+    [Inject]
+    public ISnackbar snackbar { get; set; } = null!;
+
+    [Inject]
+    public NavigationManager navigation { get; set; } = null!;
+
+    public List<Paciente> pacientes { get; set; }
+
+    protected override async Task OnInitializedAsync()
+    {
+        pacientes = await repository.GetAllAsync();
+    }
+
+    public void GoPacienteUpdate(int idPaciente)
+    {
+        navigation.NavigateTo($"/pacientes/update/{idPaciente}");
+    }
+
+    public async Task DeletePaciente(Paciente paciente)
+    {
+        try
+        {
+            var resultado = await dialog.ShowMessageBox("Atenção", $"Deseja excluir o paciente {paciente.Nome}?", yesText: "SIM", cancelText: "NÃO") ?? false;
+
+            if (resultado)
+            {
+                await repository.DeleteAsync(paciente.Id);
+
+                snackbar.Add($"Paciente excluído com sucesso.", Severity.Success);
+            }
+        }
+        catch (Exception ex)
+        {
+            snackbar.Add($"Erro contate o administrador: {ex.Message}", Severity.Error);
+        }
+    }
+}
+
